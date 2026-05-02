5f7770fbfe5855a083064b7b22ffe93f97f40f18
wthrajat <rajatkhanduri290102@gmail.com>
2026-05-03 02:01:46 +0530

    chore: remove dist folder

--- dist/src/prompt.js
+++
@@ -1,4 +0,0 @@
-export function generatePrompt(diff, type) {
-    const typeConstraint = type ? `Use type: ${type}.` : 'Determine the best conventional commit type.';
-    return `${typeConstraint}\n\nGit diff:\n${diff}`;
-}
